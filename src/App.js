2140b6aebf0ad855390997cb107b5bb6ee410674
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-05 12:12:09 +0000

    chore: maintanence

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import data from './data';
-import {useState} from 'react';
+import data from "./data";
+import { useState } from "react";
+
 function App() {
-const [selection, setSelection] = useState(0)
-const [text, setText] = useState(data)
-const [numValue, setnumValue] = useState(0)
+  const [selection, setSelection] = useState(0);
+  const [text, setText] = useState(data);
+  const [numValue, setnumValue] = useState(0);
 
+  const inputHandler = (e) => {
+    setnumValue(e.target.value);
+  };
 
-  const inputHandler=(e)=>{
-    setnumValue(e.target.value)
-  }
-  const generatePara=(e)=>{
-    e.preventDefault()
-    setSelection(numValue)
-  }
+  const generatePara = (e) => {
+    e.preventDefault();
+    setSelection(numValue);
+  };
+
   return (
     <main>
       <section className="container">
-        <header className='head'>
-            <h1>TIRED OF BORING LOREM IPSUM?</h1>
+        <header className="head">
+          <h1>TIRED OF BORING LOREM IPSUM?</h1>
         </header>
 
         <section className="inputs-container">
-
           <h2>Paragraph:</h2>
 
-          <form onSubmit={generatePara} className='form-control'>
-            <input type="number" min={0} value={numValue} onChange={inputHandler}/>
-            <button type='submit'><span>Generate</span></button>
+          <form onSubmit={generatePara} className="form-control">
+            <input
+              type="number"
+              min={0}
+              value={numValue}
+              onChange={inputHandler}
+            />
+            <button type="submit">
+              <span>Generate</span>
+            </button>
           </form>
-
         </section>
         <section className="paragraph-container">
-          {
-            text.map((para,index)=>index< parseInt(selection) ? <p key={index} className='paragraph'>{para}</p> : '')
-          }
+          {text.map((para, index) =>
+            index < parseInt(selection) ? (
+              <p key={index} className="paragraph">
+                {para}
+              </p>
+            ) : (
+              ""
+            )
+          )}
         </section>
       </section>
     </main>
